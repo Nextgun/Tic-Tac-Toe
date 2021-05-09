e26ae38256b5513f5c05e0bb215fae8555a94178
Nextgun <hmmarrero38@gmail.com>
2021-05-08 23:13:30 -0500

    what i had before making the git

---
+++ player.py
@@ -0,0 +1,13 @@
+
+class Player():
+    def __init__(self, icon):
+        self.icon = icon
+
+    def get_input(self):
+            p_input = input ("where r u gonna mark? ")
+
+
+    
+class AI():
+    def __init__(self):
+        pass
