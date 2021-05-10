4949b499a925e418798d240564148ac9794e1915
Nextgun <hmmarrero38@gmail.com>
2021-05-10 18:55:41 -0500

    cleaned code. next is pl input and draw moves.

--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 import pygame
-from logic import *
+import logic as l
+
 # player needs to create the player and handle input
-class Player():
+class HumanPlayer():
     def __init__(self, icon):
         self.icon = icon
 
@@ -11,17 +12,27 @@ class Player():
             if event.button == 1: # left mouse button
 
                 # loop through each tile available
-                for rect in Rect_list: 
+                for rect in l.Rect_list: 
+                    print("i am in the rectlist loop")
                     # check if the rect collides with the mouse pos
-                    self.check_button_collision
+                    self.check_button_collision(rect)
 
-def check_button_collision(self):
-    if rect.collidepoint(pygame.mouse.get_pos()):
-                            # if there is a collision
-                            # get tile postion data
-                            # update the board data with the new move
-                            # draw the new board
-                            pass
+    def check_button_collision(self,Rectangle):
+        print ("i am in check button collision function")
+        # if there is a collision, checks each rectangle
+        if Rectangle.collidepoint(pygame.mouse.get_pos()):
+            print(Rectangle.collidepoint) # prints out the collion info
+            print(Rectangle) # prints out rectangle info
+            # update board data here
+            
+            # use rectangle info to edit the logic rect_list and update to draw it
+                                # get tile postion data
+                                # update the board data with the new move
+                                # draw the new board
+        else:
+            print("didnt find any collision")                       
+                                
+            
             
     
 class AI():
